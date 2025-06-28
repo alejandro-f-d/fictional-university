2c942615eb77f291dd12ae15f9bc5992f65c6d29
Alejandro <alexfisac04@gmail.com>
2025-06-28 16:16:21 +0200

    Primera versión de la estructura de la página web.

---
+++ frontend/web/js/login.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputCorreo = document.getElementById('entrada-usuario-correo');
+    const correo_login = document.getElementById('correo-login');
+    const boton_login = document.getElementById('entrar');
+    const contrasenaCorreo = document.getElementById('entrada-usuario-contrasena');
+
+   
+    document.getElementById('entrar').addEventListener('click', function () {
+        console.log(inputCorreo.value);
+        console.log(contrasenaCorreo.value);
+    });
+
+});
---
+++ frontend/web/js/pagina_crear_cuenta_login.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const botonLogin = document.getElementById('login');
+    const botonCreateAccount = document.getElementById('create-account');
+
+
+
+
+    document.getElementById('login').addEventListener('click', function () {
+        window.location.href = '../assets/login.html';
+    });
+
+
+    
+});
